d24afdebbd371e2d15bf565720bfb731e412e84d
Amirreza Ektesabi <57006850+amirreza-ektesabi@users.noreply.github.com>
2023-02-18 00:40:59 +0330

    move functions to a new dir & define some new functions

---
+++ frontend/src/general/functions/objectToArray.js
@@ -0,0 +1,8 @@
+export default function objectToArray(value) {
+  if (typeof value === "object")
+    return Array.prototype.concat.apply(
+      [],
+      Array.from(Object.keys(value), (key) => value[key])
+    );
+  else return [value];
+}
---
+++ frontend/src/general/functions/stringFormat.js
@@ -0,0 +1,6 @@
+export default function stringFormat(format) {
+  var args = Array.prototype.slice.call(arguments, 1);
+  return format.replace(/{(\d+)}/g, function (match, number) {
+    return typeof args[number] != "undefined" ? args[number] : match;
+  });
+}
