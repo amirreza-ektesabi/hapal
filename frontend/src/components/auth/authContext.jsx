c5b09d0bfc26ad04d3f5866a393e3c928afda3cf
Amirreza Ektesabi <57006850+amirreza-ektesabi@users.noreply.github.com>
2023-07-07 01:19:59 +0330

    move out the auth related parts from _app

--- frontend/src/components/auth/authContext.jsx
+++ frontend/src/components/auth/authContext.jsx
@@ -1,5 +1,54 @@
 import React from "react";
+import { useRouter } from "next/router";
+import Box from "@mui/material/Box";
+import store from "src/_store";
+import Authbar from "src/components/authbar";
+import LoginDialog from "src/components/auth/loginDialog";
+import SignupDialog from "src/components/auth/signupDialog";
 
 const AuthContext = React.createContext(null);
 
 export default AuthContext;
+
+export function AuthProvider({ children }) {
+  const router = useRouter();
+
+  const [openLoginBox, setOpenLoginBox] = React.useState(false);
+  const handleOpenLoginBox = () => setOpenLoginBox(true);
+  const handleCloseLoginBox = () => setOpenLoginBox(false);
+
+  const [openSignupBox, setOpenSignupBox] = React.useState(false);
+  const handleOpenSignupBox = () => setOpenSignupBox(true);
+  const handleCloseSignupBox = () => setOpenSignupBox(false);
+
+  const state = store.getState().auth;
+  const contextValue = {
+    openLoginBox: handleOpenLoginBox,
+    openSignupBox: handleOpenSignupBox,
+    isAuthenticated: !!state.user,
+    currentUser: {
+      ...state.user,
+    },
+  };
+
+  const isHomePage = router.pathname === "/";
+  const includeAuthbar = !isHomePage && !state.user;
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {includeAuthbar && <Authbar />}
+      <Box className={includeAuthbar ? "mt-[3rem]" : ""} />
+      {children}
+      <LoginDialog
+        open={openLoginBox}
+        handleClose={handleCloseLoginBox}
+        handleOpenSignupBox={handleOpenSignupBox}
+      />
+      <SignupDialog
+        open={openSignupBox}
+        handleClose={handleCloseSignupBox}
+        handleOpenLoginBox={handleOpenLoginBox}
+      />
+    </AuthContext.Provider>
+  );
+}
--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -3,17 +3,13 @@ import "tailwindcss/tailwind.css";
 import * as React from "react";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "@mui/material/styles";
-import Box from "@mui/material/Box";
 import NoSsr from "@mui/material/NoSsr";
 import CssBaseline from "@mui/material/CssBaseline";
-import darkTheme from "src/general/theme";
 import store from "src/_store";
+import darkTheme from "src/general/theme";
 import Tabbar from "src/components/tabbar";
-import Authbar from "src/components/authbar";
 import { AlertProvider } from "src/components/alert";
-import AuthContext from "src/components/auth/authContext";
-import LoginDialog from "src/components/auth/loginDialog";
-import SignupDialog from "src/components/auth/signupDialog";
+import { AuthProvider } from "src/components/auth/authContext";
 import { pageTitle } from "src/_helpers";
 
 export default function App({ Component, pageProps, ...appProps }) {
@@ -21,50 +17,19 @@ export default function App({ Component, pageProps, ...appProps }) {
     document.title = pageTitle();
   }, []);
 
-  const [openLoginBox, setOpenLoginBox] = React.useState(false);
-  const handleOpenLoginBox = () => setOpenLoginBox(true);
-  const handleCloseLoginBox = () => setOpenLoginBox(false);
-
-  const [openSignupBox, setOpenSignupBox] = React.useState(false);
-  const handleOpenSignupBox = () => setOpenSignupBox(true);
-  const handleCloseSignupBox = () => setOpenSignupBox(false);
-
-  const authState = store.getState().auth;
-  const authContextValue = {
-    openLoginBox: handleOpenLoginBox,
-    openSignupBox: handleOpenSignupBox,
-    isAuthenticated: !!authState.user,
-    currentUser: {
-      ...authState.user,
-    },
-  };
-
   const isHomePage = appProps.router.pathname === "/";
-  const includeAuthbar = !isHomePage && !authState.user;
 
   return (
     <Provider store={store}>
       <ThemeProvider theme={darkTheme}>
         <NoSsr>
           <CssBaseline />
-          <AuthContext.Provider value={authContextValue}>
-            <AlertProvider>
-              {includeAuthbar && <Authbar />}
-              <Box className={includeAuthbar ? "mt-[3rem]" : ""} />
+          <AlertProvider>
+            <AuthProvider>
               <Component {...pageProps} />
               {!isHomePage && <Tabbar />}
-              <LoginDialog
-                open={openLoginBox}
-                handleClose={handleCloseLoginBox}
-                handleOpenSignupBox={handleOpenSignupBox}
-              />
-              <SignupDialog
-                open={openSignupBox}
-                handleClose={handleCloseSignupBox}
-                handleOpenLoginBox={handleOpenLoginBox}
-              />
-            </AlertProvider>
-          </AuthContext.Provider>
+            </AuthProvider>
+          </AlertProvider>
         </NoSsr>
       </ThemeProvider>
     </Provider>
